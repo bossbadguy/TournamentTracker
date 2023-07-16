9f36c45d729319184c5e1b0a6e047c69f991f294
BossBadGuy <bossbadguy@gmail.com>
2023-07-16 16:14:11 +0200

    Add project files.

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// Represents matchup
+        /// </summary>
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+
+        /// <summary>
+        /// Represents the winner of the matchup
+        /// </summary>
+        public TeamModel Winner { get; set; }
+
+        /// <summary>
+        /// Represents the winner of the matchup
+        /// </summary>
+        public int MatchupRound { get; set; }
+    }
+}
---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PrizeModel
+    {
+
+        /// <summary>
+        /// Represents Placement
+        /// </summary>
+        public int PlaceNumber { get; set; }
+
+        /// <summary>
+        /// Represents the prize place
+        /// </summary>
+        public string PlaceName { get; set; }
+
+        /// <summary>
+        /// Represents the amount of the prize
+        /// </summary>
+        public decimal PrizeAmount { get; set; }
+
+        /// <summary>
+        /// Represents the percentage of prize to be distributed
+        /// </summary>
+        public double PrizePercentage { get; set; }
+    }
+}
